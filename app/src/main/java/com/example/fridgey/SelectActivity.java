aeaf83d03cb892c54fa9a7a9dca31722a76bd059
mirkovicbojan <mirkovicbojan@yahoo.com>
2022-08-23 16:29:45 +0200

    Rounded Corners

--- app/src/main/java/com/example/fridgey/SelectActivity.java
+++ app/src/main/java/com/example/fridgey/SelectActivity.java
@@ -41,51 +41,7 @@ public class SelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
         //ImageView thumbnail = findViewById(R.id.cocktailThumb);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String URL = "https://thecocktaildb.com/api/json/v1/1/random.php";
-        Cocktail koktel_dana = new Cocktail();
-        queue.getCache().clear();
-        JsonObjectRequest arrayRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                URL,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
 
-                        try {
-                            JSONArray drinks = response.getJSONArray("drinks");
-                            JSONObject drink = drinks.getJSONObject(0);
-                            koktel_dana.setId(Integer.parseInt(drink.getString("idDrink")));
-                            koktel_dana.setName(drink.getString("strDrink"));
-                            koktel_dana.setImgurl(drink.getString("strDrinkThumb"));
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                    }
-                },
-                new Response.ErrorListener()
-                {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // TODO Auto-generated method stub
-                        Log.d("ERROR","error => "+error.toString());
-                    }
-                }
-        ) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<>();
-                headers.put("Content-Type", "application/json");
-                headers.put("api-key", "1");
-                return headers;
-
-            }
-        };
-        queue.add(arrayRequest);
 
     }
 }
