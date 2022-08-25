07dcf117aca050767a381ec903401e5fe5f49c4e
mirkovicbojan <mirkovicbojan@yahoo.com>
2022-08-25 11:40:29 +0200

    Added Search Activity

--- app/src/main/java/com/example/fridgey/SelectActivity.java
+++ app/src/main/java/com/example/fridgey/SelectActivity.java
@@ -4,37 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.InputStream;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class SelectActivity extends AppCompatActivity {
 
     public void proceedToOption(String option)
     {
-        Intent intent = new Intent(getApplicationContext(), CocktailDetailsActivity.class);
-        intent.putExtra("picked_option", option);
-        startActivity(intent);
+        if(option.equals("random"))
+        {
+            Intent intent = new Intent(getApplicationContext(), CocktailDetailsActivity.class);
+            intent.putExtra("picked_option", option);
+            startActivity(intent);
+        }
+        if(option.equals("byName"))
+        {
+            Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+            intent.putExtra("picked_option", option);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -43,14 +33,9 @@ public class SelectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select);
 
         CardView randomSearch = findViewById(R.id.randomSearch);
-        randomSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView optionText = findViewById(R.id.random_pick);
-                proceedToOption(optionText.getText().toString());
+        randomSearch.setOnClickListener(view -> proceedToOption("random"));
 
-            }
-        });
-
+        CardView searchByName = findViewById(R.id.searchByName);
+        searchByName.setOnClickListener(view -> proceedToOption("byName"));
     }
 }
